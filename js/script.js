fbec383d0e15fb297721f96d74e0a1acb6122b34
Ashton Walden <ashtonwalden@icloud.com>
2021-07-27 04:29:21 -0600

    added styling, html input, and restricted numbers from 1-8

--- js/script.js
+++ js/script.js
@@ -6,43 +6,86 @@ Base Assignment
     5. Style html document
 
 Additonal Content (Select 2)
-    > Limit the number the user can select to 8 or less
-        - If larger than 8, display error message
-    > Reveal the results on page using CSS animation
-        - Lookup animations
-            - super hero CSS?
-    > Use JavaScript to play sound when numbers are displayed
-        - Learn how to add sounds to function (output)
-    > Allow user to enter number via a web rather than prompt
-        - text box and then hitting enter or submit button
-            - on.click funciton?
-    > Add button that generates new numbers
-        - Doesn't ask user to specify quantity of numbers again
-            - Uses previously selected amount
+> Limit the number the user can select to 8 or less
+    - If larger than 8, display error message
+> Allow user to enter number via a web rather than prompt
+    - text box and then hitting enter or submit button
+        - on.click funciton?
+> Add button that generates new numbers
+    - Doesn't ask user to specify quantity of numbers again
+        - Uses previously selected amount
 */  
 
 // Prompts the user to enter how many numbers they would like generated and sets it equal to userNum
-var userNum = prompt("How many numbers?");
+                    // var userNum = prompt("How many numbers?");
+
+
+
+
+
 // array to hold lottery numbers
 var lottoNums = [];
 // variable to hold lottery picks and help format for html display
 var lottery = "";
 
-// generates the selected amount of random numbers
-for (i = 0; i < userNum; i++) {
-    // passes randomly generated numbers into lottoNums array
-    lottoNums[i] = Math.floor(Math.random() * 99);
+
+// function lottoFunction() {
+//     // generates the selected amount of random numbers
+//     for (i = 0; i < userNum; i++) {
+//         // passes randomly generated numbers into lottoNums array
+//         lottoNums[i] = Math.floor(Math.random() * 99);
+//     }
+
+//     for (i = 0; i < lottoNums.length; i++) {
+//         // checks to see if it is the last time looping through, if so don't add "-" after num
+//         if ([i] == lottoNums.length-1) {
+//             // doesn't add dash after num
+//             lottery += lottoNums[i];
+//         } else {
+//             // separates lottery numbers by dashes (same as lottery = lottery + lottoNums[i] + "-";)
+//             lottery += lottoNums[i] + " - "
+//         }
+//     }
+// }
+
+function test() { 
+    document.getElementById("test-div").innerHTML = "Test success!!!!!!!!"
 }
 
-for (i = 0; i < lottoNums.length; i++) {
-    // checks to see if it is the last time looping through, if so don't add "-" after num
-    if ([i] == lottoNums.length-1) {
-        // doesn't add dash after num
-        lottery += lottoNums[i];
+
+
+function lotteryFunction() {
+    var userNum = document.getElementById("num-input").value;
+
+    if(userNum < 1 || userNum > 8) {
+        alert("Please choose a number between 1 & 8!")
     } else {
-        // separates lottery numbers by dashes (same as lottery = lottery + lottoNums[i] + "-";)
-        lottery += lottoNums[i] + " - "
+        // generates the selected amount of random numbers
+        for (i = 0; i < userNum; i++) {
+            // passes randomly generated numbers into lottoNums array
+            lottoNums[i] = Math.floor(Math.random() * 99);
+        }
+
+        for (i = 0; i < lottoNums.length; i++) {
+            // checks to see if it is the last time looping through, if so don't add "-" after num
+            if ([i] == lottoNums.length-1) {
+                // doesn't add dash after num
+                lottery += lottoNums[i];
+            } else {
+                // separates lottery numbers by dashes (same as lottery = lottery + lottoNums[i] + "-";)
+                lottery += lottoNums[i] + " - "
+            }
+        }
     }
+
+    document.getElementById("lottoDisplay").innerHTML = lottery;
+
+    
 }
 
-document.getElementById("lottoDisplay").innerHTML = lottery;
+
+
+// lottoFunction();
+
+// document.getElementById("lottoDisplay").innerHTML = lottery;
+
